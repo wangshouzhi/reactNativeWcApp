ec2dfc3550904f56bb4db92088c7d44d6dae1e3e
wsz <wsz007007@163.com>
2019-08-28 22:30:23 +0800

    0828 init

--- reactNativeWcApp/routes/data.js
+++ reactNativeWcApp/routes/data.js
@@ -33,7 +33,7 @@ router.get('/read', function(req, res, next) {
 
 //数据存储模块
 
-router.get('/write', (req, res, next) => {
+router.post('/write', function(req, res, next) {
     // 文件名
     var type = req.param('type') || '';
     // 关键字段
@@ -47,7 +47,8 @@ router.get('/write', (req, res, next) => {
         })
     }
     //1读取文件
-    fs.readFile(PATH + type + '.json', function(err,data) {
+    var filePath = PATH + type + '.json';
+    fs.readFile(filePath, function(err, data) {
         if(err) {
             return res.send({
                 status: 0,
@@ -58,14 +59,32 @@ router.get('/write', (req, res, next) => {
         var obj = {
             img: img,
             url: url,
-            title,
-            id: ,
+            title: title,
+            id: guidGenerate(),
             time: new Date()
-        }
-    })
-})
-
+        };
+        arr.splice(0, 0, obj);
+        //2写入数据
+        var newData =JSON.stringify(arr);
+        fs.writeFile(filePath, newData, function(err) {
+            if(err) {
+                return res.send({
+                    status: 0,
+                    info: '写入数据失败'
+                });
+            }
+            return res.send({
+                status: 1,
+                info: obj
+            });
+        });
+    });    
+});
 function guidGenerate() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c))
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    }).toUpperCase()
 }
 module.exports = router;
